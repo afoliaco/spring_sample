272b639e0594e306d6fb6238433f8edc783afa24
Adolfo Foliaco <adolfo.foliaco@gmail.com>
2018-05-28 21:15:09 -0400

    starting point for spring course pluralsight

---
+++ src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -0,0 +1,23 @@
+package com.pluralsight.repository;
+
+import com.pluralsight.model.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HibernateCustomerRepositoryImpl implements CustomerRepository {
+
+    @Override
+    public List<Customer> findAll(){
+
+        List<Customer> customers = new ArrayList<>();
+
+        Customer customer = new Customer();
+        customer.setFirstName("Adolfo");
+        customer.setLastName("Foliaco");
+        customers.add(customer);
+
+        return  customers;
+
+    }
+}
