c27ac20e1bdc64a051fdff133fc308bb4179ca18
Adolfo Foliaco <adolfo.foliaco@gmail.com>
2018-05-28 21:31:44 -0400

    testing singed key

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,6 +6,6 @@ public class Application {
     public static void main(String args[]) {
 
         CustomerService service = new CustomerServiceImpl();
-        System.out.println(service.findAll().get(0).getFirstName());
+        System.out.println(service.findAll().get(0).getFirstName()); 
     }
 }
